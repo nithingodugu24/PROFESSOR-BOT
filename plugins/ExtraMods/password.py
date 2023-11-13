4b60d4cd5d7da05f9c699e033c5db63dfe9ad428
nithingodugu24 <85222889+nithingodugu24@users.noreply.github.com>
2023-11-13 20:11:48 +0530

    added server stattistis webpage

---
+++ plugins/ExtraMods/password.py
@@ -0,0 +1,6 @@
+import random, os
+from pyrogram import Client, filters, enums 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,16 +1,51 @@
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums 
 from database.users_chats_db import db
 from info import SUPPORT_CHAT
 from aiohttp import web
 from utils import temp
-
+import logging, re, asyncio, time, shutil, psutil, os, sys
+from datetime import datetime
+import json
+# Helper Function
+from Script import script
+from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings, get_shortlink, get_time, humanbytes 
+from .ExtraMods.carbon import make_carbon
+import info
 
 routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
     return web.json_response(text='{"status":"running","dev":"hasiba518738"}')
+
+@routes.get("/helloWorld", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response(text='{"success":true,"message":"Hello World !"}')
+
+@routes.get("/serverStatistics", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response(text='{"success":true,"message":"Hello World !"}')
+
+
+@routes.get("/serverStatistics", allow_head=True)
+async def server_statistics_handler(request):
+    total, used, free = shutil.disk_usage(".")
+    stats = {
+        "uptime": get_time(time.time() - info.UPTIME),
+        "cpu_percent": psutil.cpu_percent(),
+        "memory_percent": psutil.virtual_memory().percent,
+        "total_disk_space": humanbytes(total),
+        "used_disk_space": humanbytes(used),
+        "disk_usage_percent": psutil.disk_usage('/').percent,
+        "free_disk_space": humanbytes(free)
+    }
+    return web.json_response(text=json.dumps(stats))
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
