32cfcf90c0ac817f989e1dcf8a797459b03331d4
nithingodugu24 <85222889+nithingodugu24@users.noreply.github.com>
2023-11-21 15:52:11 +0530

    revindmain

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ class Bot(Client):
         if WEBHOOK is True:
             app = web.AppRunner(await web_server())
             await app.setup()
-            await web.TCPSite(app, "0.0.0.0", 8081).start()
+            await web.TCPSite(app, "0.0.0.0", 8080).start()
             logger.info("Web Response Is Running......🕸️")
             
     async def stop(self, *args):
